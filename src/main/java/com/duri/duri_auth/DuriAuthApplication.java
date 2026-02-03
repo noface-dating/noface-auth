671fe55893799c675901da8e452349538c859edf
chaewonn <scw0629@naver.com>
2026-02-03 13:40:12 +0900

    init

---
+++ src/main/java/com/duri/duri_auth/DuriAuthApplication.java
@@ -0,0 +1,13 @@
+package com.duri.duri_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuriAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuriAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duri/duri_auth/DuriAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duri.duri_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuriAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
