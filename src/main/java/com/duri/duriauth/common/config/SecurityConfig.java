4df663ac5050d004e4d2f6f510379a10409010ad
YeonWooKimm <kyw10987@gmail.com>
2026-04-19 21:34:27 +0900

    fix:왜 Bcrypt를 추가를...

--- src/main/java/com/duri/duriauth/common/config/SecurityConfig.java
+++ src/main/java/com/duri/duriauth/common/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -40,9 +41,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // TODO: Password Encoder 적용
-        return NoOpPasswordEncoder.getInstance();   // 테스트 위해 Encoder 비활성화
-//        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
